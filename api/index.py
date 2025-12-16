08fde6742d8690c3d37482cf70c1b702d10ca563
Ala1 <alauantdr@gmail.com>
2025-12-16 16:31:39 -0300

    conserto

--- api/index.py
+++ api/index.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI
-from mangum import Mangum
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"status": "Online"}
+    return {"status": "Online", "msg": "Funcionando nativamente!"}
 
-@app.post("/validar_qr")
-def validar(dados: dict):
-    return {"recebido": dados}
-
-handler = Mangum(app)
+@app.get("/validar/{codigo}")
+def validar(codigo: str):
+    return {"recebido": codigo, "acesso": "permitido"}
