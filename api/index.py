00b064e65814e74d7feb285c4a9768eaac07bf16
Ala1 <alauantdr@gmail.com>
2025-12-18 01:38:58 -0300

    conserto

--- api/index.py
+++ api/index.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, Depends, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from starlette.middleware.sessions import SessionMiddleware 
+from bson import ObjectId, errors
 import os
-from .database import lifespan
+from .database import lifespan, get_db
 from .utils import verificar_login_global
 from .routes import auth, view, change
 
@@ -23,6 +24,16 @@ async def pagina_inicial(request: Request):
     owner_id = request.session.get("usuario_logado")
     if not owner_id:
         return RedirectResponse(url="/auth/login", status_code=303)
+    try:
+        oid = ObjectId(owner_id)
+        
+        if not get_db().owners.find_one({"_id": oid}):
+            raise ValueError("Usuário deletado ou inexistente")
+            
+    except (errors.InvalidId, ValueError, TypeError):
+        request.session.clear()
+        return RedirectResponse(url="/auth/login", status_code=303)
+
     return RedirectResponse(url=f"/view/any/{owner_id}", status_code=303)
 
 
